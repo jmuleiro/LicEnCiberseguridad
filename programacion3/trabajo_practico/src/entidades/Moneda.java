4123e59bd9f9a7d49f27c6aa07659383958a0eee
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-10-19 21:25:36 -0300

    clase moneda

---
+++ programacion3/trabajo_practico/src/entidades/Moneda.java
@@ -0,0 +1,28 @@
+package programacion3.trabajo_practico.src.entidades;
+
+public class Moneda {
+  //* Atributos
+  private String codigo;
+  private String nombre;
+
+  //* Constructores
+  public Moneda(String codigo, String nombre) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+  }
+
+  //* Getters & Setters
+  public String getCodigo() {
+    return this.codigo;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+
+  //* Métodos
+  @Override
+  public String toString() {
+    return this.nombre;
+  }
+}
