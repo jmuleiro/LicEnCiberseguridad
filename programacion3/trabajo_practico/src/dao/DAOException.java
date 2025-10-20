48dae1af957c44b2f8ddcff3b0b53465525f8c5b
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-10-20 09:08:30 -0300

    opcionalmente pasar el nombre de la clase DAO a DAOException

--- programacion3/trabajo_practico/src/dao/DAOException.java
+++ programacion3/trabajo_practico/src/dao/DAOException.java
@@ -1,6 +1,9 @@
 package programacion3.trabajo_practico.src.dao;
 
 public class DAOException extends Exception {
+  //* Atributos
+  private String daoClassName = null;
+  
   //* Constructores
   public DAOException() {
     super();
@@ -8,6 +11,16 @@ public class DAOException extends Exception {
 
   public DAOException(String mensaje) {
     super(mensaje);
+  }
+
+  public DAOException(String mensaje, String dao) {
+    super(mensaje);
+    this.daoClassName = dao;
+  }
+
+  //* Getters & Setters
+  public String getDaoClassName() {
+    return this.daoClassName;
   }
 
   //* Métodos
