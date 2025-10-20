bd5af013eb8f501032a4a56355ed909612a9cb1e
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-10-20 09:07:07 -0300

    hacer que DAOTemplate.execute pueda devolver el tipo indicado T, para que sea compatible con cualquier funcion CRUD

--- programacion3/trabajo_practico/src/dao/DAOAction.java
+++ programacion3/trabajo_practico/src/dao/DAOAction.java
@@ -2,7 +2,7 @@ package programacion3.trabajo_practico.src.dao;
 
 import java.sql.SQLException;
 
-@FunctionalInterface
-public interface DAOAction {
-  void execute() throws SQLException;
+@FunctionalInterface // T is the return type
+public interface DAOAction<T> {
+  T execute() throws SQLException;
 }
--- programacion3/trabajo_practico/src/dao/DAOTemplate.java
+++ programacion3/trabajo_practico/src/dao/DAOTemplate.java
@@ -3,11 +3,11 @@ package programacion3.trabajo_practico.src.dao;
 import java.sql.SQLException;
 
 // Similar a un Callback, esta clase permite reutilizar el bloque try/catch
-public class DAOTemplate {
-  public static void execute(String nombreElemento, DAOAction action) throws DAOException {
+public class DAOTemplate<T> {
+  public T execute(String nombreElemento, DAOAction<T> action) throws DAOException {
     try {
       // Ejecuta la accion, cualquiera sea
-      action.execute();
+      return action.execute();
     } catch (SQLException e) {
       System.out.println("SQLException: " + e.getMessage());
       throw new DAOException("Fallo de SQL en el DAO de: " + nombreElemento);
