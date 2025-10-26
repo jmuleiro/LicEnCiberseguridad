772bb1822a17b254dd79fcd014025aa6c5e13201
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-10-26 18:15:29 -0300

    actualizar el schema con las tablas faltantes

--- programacion3/trabajo_practico/schema.sql
+++ programacion3/trabajo_practico/schema.sql
@@ -20,6 +20,10 @@ CREATE TABLE Moneda (
     PRIMARY KEY (cod_moneda)
 );
 
+-- Cargar Monedas
+INSERT INTO Moneda VALUES ('ARS', 'Pesos Argentinos');
+INSERT INTO Moneda VALUES ('USD', 'Dolares');
+
 /* Tipo de Cuenta */
 CREATE TABLE Tipo_Cuenta (
     cod_tipo_cuenta CHAR(3) NOT NULL,
@@ -31,9 +35,16 @@ CREATE TABLE Tipo_Cuenta (
 INSERT INTO Tipo_Cuenta VALUES ('COR', 'Cuenta Corriente');
 INSERT INTO Tipo_Cuenta VALUES ('SAV', 'Caja de Ahorro');
 
--- Cargar Monedas
-INSERT INTO Moneda VALUES ('ARS', 'Pesos Argentinos');
-INSERT INTO Moneda VALUES ('USD', 'Dolares');
+/* Tipo de Tarjeta */
+CREATE TABLE Tipo_Tarjeta (
+    cod_tipo_tarjeta CHAR(3) NOT NULL,
+    nombre_tipo CHAR(50) NOT NULL,
+    PRIMARY KEY (cod_tipo_tarjeta)
+);
+
+-- Cargar Tipos
+INSERT INTO Tipo_Tarjeta VALUES ('CRE', 'Credito');
+INSERT INTO Tipo_Tarjeta VALUES ('DEB', 'Debito');
 
 /* Usuario */
 CREATE TABLE Usuario (
@@ -75,6 +86,87 @@ CREATE TABLE Cuenta (
     -- Foreign Key 3: Moneda
     FOREIGN KEY (cod_moneda)
         REFERENCES Moneda(cod_moneda)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+/* Tarjeta */
+CREATE TABLE Tarjeta (
+    tarjeta_id INT NOT NULL AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    cod_tipo_tarjeta CHAR(3) NOT NULL,
+    limite DOUBLE NOT NULL,
+    numero INT NOT NULL,
+    fecha_vencimiento DATE NOT NULL,
+    cvc INT NOT NULL,
+    PRIMARY KEY (tarjeta_id),
+    
+    -- Foreign Key 1: Usuario
+    FOREIGN KEY (usuario_id)
+        REFERENCES Usuario(usuario_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+        
+    -- Foreign Key 2: Tipo de Tarjeta
+    FOREIGN KEY (cod_tipo_tarjeta)
+        REFERENCES Tipo_Tarjeta(cod_tipo_tarjeta)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+/* Consumo */
+CREATE TABLE Consumo (
+    tarjeta_id INT NOT NULL,
+    consumo_id INT NOT NULL,
+    fecha DATE NOT NULL,
+    cantidad DOUBLE NOT NULL,
+    cod_moneda CHAR(3) NOT NULL,
+    PRIMARY KEY (tarjeta_id, consumo_id),
+    
+    -- Foreign Key 1: Tarjeta
+    FOREIGN KEY (tarjeta_id)
+        REFERENCES Tarjeta(tarjeta_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+        
+    -- Foreign Key 2: Moneda
+    FOREIGN KEY (cod_moneda)
+        REFERENCES Moneda(cod_moneda)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+/* Transferencia */
+CREATE TABLE Transferencia (
+    transferencia_id INT NOT NULL AUTO_INCREMENT,
+    monto DOUBLE NOT NULL,
+    cod_moneda CHAR(3) NOT NULL,
+    concepto CHAR(50) NOT NULL,
+    PRIMARY KEY (transferencia_id),
+    
+    -- Foreign Key: Moneda
+    FOREIGN KEY (cod_moneda)
+        REFERENCES Moneda(cod_moneda)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+);
+
+/* Movimientos */
+CREATE TABLE Movimientos (
+    transferencia_id INT NOT NULL,
+    cuenta_id INT NOT NULL,
+    entrante BOOLEAN NOT NULL, -- TRUE para transferencias entrantes, FALSE para salientes
+    PRIMARY KEY (transferencia_id, cuenta_id),
+    
+    -- Foreign Key 1: Transferencia
+    FOREIGN KEY (transferencia_id)
+        REFERENCES Transferencia(transferencia_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+        
+    -- Foreign Key 2: Cuenta
+    FOREIGN KEY (cuenta_id)
+        REFERENCES Cuenta(cuenta_id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
