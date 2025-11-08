a039dd1ecfc106b3e8de5fcc6997cf8ea5e28819
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-11-08 18:07:04 -0300

    corregir throws y return type

--- programacion3/trabajo_practico/src/dao/DAOTarjetaCredito.java
+++ programacion3/trabajo_practico/src/dao/DAOTarjetaCredito.java
@@ -14,13 +14,13 @@ public class DAOTarjetaCredito extends DAOBase<TarjetaCredito, Integer> {
   }
 
   @Override
-  public void insertar(TarjetaCredito elemento) throws DAOException {
+  public void insertar(TarjetaCredito elemento) throws UnsupportedOperationException {
     // Se deja este método implementado para compatibilidad
     throw new UnsupportedOperationException("El método insertar requiere el usuario como parámetro");
   }
 
   public void insertar(TarjetaCredito elemento, UsuarioCliente usuario) throws DAOException {
-    new DAOTemplate<TarjetaCredito>().execute(entityName, () -> {
+    new DAOTemplate<Void>().execute(entityName, () -> {
       PreparedStatement preparedStatement = conn.prepare(
         "INSERT INTO Tarjeta (usuario_id, cod_tipo_tarjeta, limite, numero, fecha_vencimiento, cvc)" + 
         "VALUES (?, \"CRE\", ?, ?, ?, ?)"
