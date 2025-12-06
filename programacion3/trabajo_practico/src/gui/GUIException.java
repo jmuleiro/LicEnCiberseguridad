dc54f3f9e33cb7db42cc3d296de708ce0c43f4c9
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-12-06 18:54:26 -0300

    GUIException

---
+++ programacion3/trabajo_practico/src/gui/GUIException.java
@@ -0,0 +1,10 @@
+package programacion3.trabajo_practico.src.gui;
+
+public class GUIException extends Exception {
+  public GUIException() {
+  }
+
+  public GUIException(String message) {
+    super(message);
+  }
+}
