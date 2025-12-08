c8927fc179b5761afe38bd34d600f29ad2ef02a6
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2025-12-07 21:00:46 -0300

    solucionar posicionamiento de los paneles al navegar

--- programacion3/trabajo_practico/src/gui/PanelManager.java
+++ programacion3/trabajo_practico/src/gui/PanelManager.java
@@ -10,18 +10,17 @@ import javax.swing.JPanel;
 public class PanelManager {
   JFrame jFrame;
 
-  public PanelManager(int tipo) {
-    jFrame = new JFrame();
-    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    mostrar(tipo);
-  }
-
   public void mostrar(JPanel panel) {
     jFrame.getContentPane().removeAll();
     jFrame.getContentPane().add(BorderLayout.CENTER, panel);
     jFrame.getContentPane().validate();
     jFrame.getContentPane().repaint();
-    jFrame.pack();
+  }
+
+  public PanelManager(int tipo) {
+    jFrame = new JFrame();
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mostrar(tipo);
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
