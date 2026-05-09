cb684fbb9c07eaddbcefcb1014d68808b6504e59
Joaquín Muleiro <joaquinmuleirobeltran@gmail.com>
2026-05-09 13:53:06 -0300

    clase 9

---
+++ arquitectura_web/clase9/main.js
@@ -0,0 +1,53 @@
+// Imports
+import * as THREE from 'three';
+import { TextGeometry } from 'three/examples/jsm/Addons.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+
+// Escena
+const scene = new THREE.Scene();
+
+// Camara
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 75;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Variables
+let textMesh, fuente;
+
+cargarFuente();
+
+// Carga fuente para el texto
+function cargarFuente() {
+  const fontLoader = new FontLoader();
+  fontLoader.load("https://threejs.org/examples/fonts/helvetiker_regular.typeface.json", (respuesta) => {
+    fuente = respuesta;
+    crearTexto();
+  });
+}
+
+// Inicializa el texto
+function crearTexto() {
+  const textGeometry = new TextGeometry("Joaquin", {
+    font: fuente,
+    size: 7,
+    height: 0.5,
+    depth: 1.5
+  });
+
+  textGeometry.computeBoundingBox();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  textMesh = new THREE.Mesh(textGeometry, material);
+  textMesh.position.set(-15, 0, 0);
+  scene.add(textMesh);
+  renderer.setAnimationLoop(animar);
+}
+
+// Animacion
+function animar(time) {
+  textMesh.position.y = Math.sin(time / 300) * 20;
+  renderer.render(scene, camera);
+}
